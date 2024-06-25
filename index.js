623705557d0cd5d6ac6e67a54eeff55ad1e8c682
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-06-25 20:56:54 +0000

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'qr.html'));
+    res.sendFile(path.join(__dirname, 'public', '/qr.html'));
 });
 
 app.listen(PORT, () => {
