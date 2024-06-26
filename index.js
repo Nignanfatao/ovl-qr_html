6fbcf98e07f06d8fbb1c50756034e2b8746042cc
Fatao <140420451+Nignanfatao@users.noreply.github.com>
2024-06-26 08:46:47 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/qr', (req, res) => {
    res.send(dataStore.getQRData());
+   console.log('lien qr:',dataStore.getQRData)
 });
 
 app.use('/', async (req, res, next) => {
